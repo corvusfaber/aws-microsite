e446c8ed339da3b6a9ee1b1dd33c3f07925c54a0
corvusfaber <cfaber2004@yahoo.com>
2024-08-08 16:57:43 +0200

    Moved terraform artifacts and added github actions workflow.

--- src/main.tf
+++ src/main.tf
@@ -8,6 +8,7 @@ terraform {
 
   backend "s3" {
     bucket  = "mlclmfrsr-tfstate"
+    dynamodb_table = "terraform-state-lock-dynamo"
     key     = "state/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
